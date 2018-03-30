4d3056e5bbd406dbce3315c82664eb59bb2349b4
Dark Tornado <dt3141592@naver.com>
2018-03-30 11:47:19 +0900

    Change License
    LGPL 3.0 -> Apache License

--- 0. 자동 학습 & 아무말 대잔치.js
+++ 0. 자동 학습 & 아무말 대잔치.js
@@ -17,24 +17,21 @@ Project - M
 
 <라이선스>
 
-이 소스에는 LGPL 3.0이 적용되어있습니다.
+이 소스에는 아파치 라이선스 2.0이 적용되어있습니다.
 
-one line to give the library's name and an idea of what it does.
-Copyright (C) 2018  Dark Tornado
+Copyright 2018 Dark Tornado
 
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
+    http://www.apache.org/licenses/LICENSE-2.0
 
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
 
 /*상수 선언. 변수여도 상관 X*/
